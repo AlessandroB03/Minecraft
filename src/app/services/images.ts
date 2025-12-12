85bc2391e8d9f578bfa20d40151457c370c8df4b
JunPrime <nestore.gonzalezf1707@gmail.com>
2025-12-12 08:59:14 -0500

    Final Version gracias come galletas

--- src/app/services/images.ts
+++ src/app/services/images.ts
@@ -18,25 +18,25 @@ export class Images {
       id: 1,
       nombre: 'Cueva',
       descripcion : 'Las cuevas en Minecraft son sistemas subterráneos llenos de minerales, oscuridad y criaturas hostiles. Son esenciales para la minería y la exploración.',
-      imagen: '/img/cueva.jpg'
+      imagen: './img/cueva.jpg'
     },
     {
       id: 2,
       nombre: 'Selva',
       descripcion: 'El bioma de selva es denso y exuberante, con árboles gigantes, lianas y abundante vegetación. Aquí aparecen loros, ocelotes y templos ocultos.',
-      imagen: '/img/selva.jpg'
+      imagen: './img/selva.jpg'
     },
     {
       id: 3,
       nombre: 'Montaña nevada',
       descripcion: 'Las montañas nevadas presentan picos altos cubiertos de nieve y hielo. Son frías y peligrosas, con cabras y un terreno escarpado ideal para retos de supervivencia.',
-      imagen: '/img/montaña.jpg'
+      imagen: './img/montaña.jpg'
     },
     {
       id: 4,
       nombre: 'Pantano',
       descripcion: 'El bioma de pantano es húmedo y fangoso, con agua poco profunda, lirios y árboles oscuros. Aquí habitan brujas en chozas y abundan los slimes durante la noche.',
-      imagen: '/img/pantano.jpg'
+      imagen: './img/pantano.jpg'
     }
   ]
 
--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -20,28 +20,28 @@ export class ProductsService {
       nombre: 'Espada de Diamante',
       descripcion: 'Arma poderosa para combatir mobs y jugadores.',
       precio: 150,
-      imagen: '/img/espadadediamante.png'
+      imagen: './img/espadadediamante.png'
     },
     {
       id: 2,
       nombre: 'Pico de Hierro',
       descripcion: 'Herramienta esencial para minar minerales.',
       precio: 80,
-      imagen: '/img/pico_hierro.webp'
+      imagen: './img/pico_hierro.webp'
     },
     {
       id: 3,
       nombre: 'Manzana Dorada',
       descripcion: 'Objeto especial que otorga regeneración y resistencia.',
       precio: 50,
-      imagen: '/img/manzana_dorada.png'
+      imagen: './img/manzana_dorada.png'
     },
     {
       id: 4,
       nombre: 'Bloque de Redstone',
       descripcion: 'Ideal para construir mecanismos y circuitos.',
       precio: 30,
-      imagen: '/img/bloque_redstone.webp'
+      imagen: './img/bloque_redstone.webp'
     }
   ];
 
